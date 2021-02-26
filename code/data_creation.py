a6e5769e52c85bea224626fc874dccab14b7c862
Marc Aurele Divernois <79636348+marcaureledivernois@users.noreply.github.com>
2021-02-26 15:18:13 +0100

    Update data_creation.py

--- code/data_creation.py
+++ code/data_creation.py
@@ -9,7 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-###################################### fake data creation ##############################################################
+############################################ simulation  ##############################################################
 
 feature_size = 12
 
